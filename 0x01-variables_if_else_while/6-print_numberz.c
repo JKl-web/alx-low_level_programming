824249fc681e61bb1a664b798c315c4830980e0a
Limo <technophileltd467@gmail.com>
2023-02-17 04:57:44 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: Always 0
+ */
+int main(void)
+{
+	int k;
+
+	k = 0;
+
+	while
+		(k < 10)
+		{
+			putchar(k + '0');
+			k++;
+		}
+
+	putchar('\n');
+
+	return (0);
+}
