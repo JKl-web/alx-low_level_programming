e210c8390cbb06d6c51a3cf117a2b263af0dd568
Limo <technophileltd467@gmail.com>
2023-02-21 04:56:01 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Make alphabets x10 times
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+
+{
+	char c;
+	int i;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+			_putchar('\n');
+	}
+}
