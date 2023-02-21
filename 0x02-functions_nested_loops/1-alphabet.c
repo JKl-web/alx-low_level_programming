69f17d6903475bbc1c1186b63046297cd9a60807
Limo <technophileltd467@gmail.com>
2023-02-21 04:32:46 -0800

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - printing alphabets in lower case
+ * Reurn: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a' ; j <= 'z' ; j++)
+		_putchar(j);
+
+	_putchar('\n');
+}
