f595e33b743e5acf308058c3155feb3719515c70
Limo <technophileltd467@gmail.com>
2023-02-21 05:56:20 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,11 @@ void print_alphabet_x10(void)
 	char c;
 	int i;
 
-	while (i <= 9)
+	for (i = 0; i <= 9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
-		{
 			_putchar(c);
-		}
-			_putchar('\n');
-			i++;
+
+		_putchar ('\n');
 	}
 }
