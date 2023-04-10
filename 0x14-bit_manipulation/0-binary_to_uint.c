224ba29fec91dc2fe8544dd4a9a7e0b8e29fde94
Limo <technophileltd467@gmail.com>
2023-04-10 00:40:46 -0700

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * int binary_to_uint - Convert a binary number to unsigned int
+ * @b: String containing bainary number
+ * Return: the converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int k;
+	unsigned int dec_val = 0;
+
+	if (!b)
+		return (0);
+	for (k = 0; b[k]; k++)
+	{
+		if (b[k] < '0' || b[k] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[k] - '0');
+	}
+	return (dec_val);
+}
