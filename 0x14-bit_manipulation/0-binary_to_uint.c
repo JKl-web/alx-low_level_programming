727f6d413de17ef4d1f37eeed5d90a4d09c78b55
Limo <technophileltd467@gmail.com>
2023-04-10 01:53:07 -0700

    Task 0.0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,7 +16,7 @@ unsigned int binary_to_uint(const char *b)
 	for (k = 0; b[k]; k++)
 	{
 		if (b[k] < '0' || b[k] > '1')
-			return (1);
+			return (0);
 		dec_val = 2 * dec_val + (b[k] - '0');
 	}
 	return (dec_val);
