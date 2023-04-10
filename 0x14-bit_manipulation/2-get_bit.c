d1603b15f064e87503e182c46a0d9e4481e8dda9
Limo <technophileltd467@gmail.com>
2023-04-10 02:27:43 -0700

    Task 2.10

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
-		return (-10);
+		return (-1);
 
 	if ((n & (1 << index)) == 0)
 		return (0);
